a34608812c78820ea4ce3c7d1b132f01873e522a
EmilyHenken <20henkee@corjesu.org>
2018-06-19 19:28:13 +0000

    automatically backed up by learn

---
+++ until.rb
@@ -0,0 +1,12 @@
+# Until loop
+
+def using_until
+  counter = 6
+  
+  until counter == 10
+    puts "Wingardium Leviosa"
+    counter = counter + 1
+    puts "Level: #{counter}."
+  end
+end
+using_until
